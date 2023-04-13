afdd12dfcdb5d825e96db5111ca39bc62944742f
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-04-13 05:33:48 -0300

    fixed compareBoard func error and updated history & game container styles to padding and btn

--- src/utils/compareBoard.js
+++ src/utils/compareBoard.js
@@ -1,6 +1,4 @@
 export const compareBoard = (board, lastBoard) => {
-  if (!lastBoard) return true;
-
   const isSameBoard = board
     .map((el, i) => el === lastBoard[i])
     .every((el) => el === true);
