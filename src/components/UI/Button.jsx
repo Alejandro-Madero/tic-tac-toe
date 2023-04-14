015c091dea0a98daf30f2309c0954b336c0f119e
Alejandro Madero <88248533+Alejandro-Madero@users.noreply.github.com>
2023-04-14 02:16:52 -0300

    Added hover specific effect for mobiles and desktop to avoid sticky hovering on touch devices

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import styles from "./Button.module.css";
 
 const Button = ({ children, onClick, className }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleTouchStart = () => setIsHovered(true);
+  const handleTouchEnd = () => setIsHovered(false);
+
   return (
-    <button className={`${styles.btn} ${className}`} onClick={onClick}>
+    <button
+      className={`${styles.btn} ${className} ${
+        isHovered ? styles["mobile-hover"] : ""
+      }
+       `}
+      onClick={onClick}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
       {children}
     </button>
   );
